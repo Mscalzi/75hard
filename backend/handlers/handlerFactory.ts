4e7b0725a94acc9d74de297b00711167796e4187
leftyxiv <manny@aledoux.net>
2022-06-16 09:36:54 -0400

    added the rest of the static handler factory functions for every model

--- backend/handlers/handlerFactory.ts
+++ backend/handlers/handlerFactory.ts
@@ -1,12 +1,13 @@
 import { Models } from '../models';
 import { Request, Response, NextFunction } from 'express';
 import { catchAsync } from '../utils/catchAsync';
+import { AppError } from '../utils/AppError';
 
 export const getOne = (Model: Models) => {
   catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const model = await Model.findById(req.params.id);
     if (!model) {
-      return res.status(404).send('Not found');
+      return next(new AppError(`No ${Model.modelName} found with that ID`, 404));
     }
     return res.status(200).json({
       status: 'success',
@@ -29,7 +30,7 @@ export const createOne = async (Model: Models) => {
   catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const model = await Model.create(req.body);
     if (!model) {
-      return res.status(400).send('Bad request');
+      return next(new AppError(`No ${Model.modelName} found with that ID`, 404));
     }
     return res.status(201).json({
       status: 'success',
@@ -37,3 +38,29 @@ export const createOne = async (Model: Models) => {
     });
   });
 };
+
+export const deleteOne = async (Model: Models) => {
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const model = await Model.findByIdAndDelete(req.params.id);
+    if (!model) {
+      return next(new AppError(`No ${Model.modelName} found with that ID`, 404));
+    }
+    return res.sendStatus(204);
+  });
+};
+
+export const updateOne = async (Model: Models) => {
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const model = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!model) {
+      return next(new AppError(`No ${Model.modelName} found with that ID`, 404));
+    }
+    return res.status(200).json({
+      status: 'success',
+      data: model,
+    });
+  });
+}
